a13349dfce99702315417b6af99c515cacfadc04
abdullah2776 <aboodqubbaj2005@gmail.com>
2025-12-05 15:38:36 +0300

    lab solution

---
+++ me/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace me.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
